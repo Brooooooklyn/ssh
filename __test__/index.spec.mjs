90196147e35dba22dd626a82ff5811d1dfa35459
LongYinan <lynweklm@gmail.com>
2024-05-16 16:15:27 +0800

    ci: update components (#23)

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -3,5 +3,9 @@ import test from 'ava'
 import { connect } from '../index.js'
 
 test('connection failed without auth', async (t) => {
-  await t.throwsAsync(() => connect('github.com:22'))
+  if (process.platform !== 'darwin') {
+    await t.throwsAsync(() => connect('github.com:22'))
+  } else {
+    await t.notThrowsAsync(() => connect('github.com:22'))
+  }
 })
